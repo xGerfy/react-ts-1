076dce5cf9caaa8b4f00e89e5b028fbe7c0e31e5
Roman Ponomarev <xgerfy@gmail.com>
2024-10-01 12:28:23 +0400

    add some components and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import Card, { CardVariant } from "./components/Card"
+import UserList from "./components/UserList"
+import { IUser } from "./types/types"
 
 const App = () => {
+  const users: IUser[] = [
+    { id: 1, name: 'xGerfy', email: 'asf@mail.ru', address: { city: 'Moscow', street: 'Lenina', zipcode: "123456" } },
+    { id: 2, name: 'roman', email: 'asf@mail.ru', address: { city: 'Izhevsk', street: 'Lenina', zipcode: "654321" } },
+  ]
+
   return (
     <div>
-      it worsk
-    </div>
+      <Card variant={CardVariant.outlined} height="200px" width="200px">
+        <button>Кнопка</button>
+      </Card>
+      <UserList users={users} />
+    </div >
   )
 }
 
